2a4958dbf9ee6a4ea81283ade3c9edda8785ad8a
Osi2 <Yegor.Osipov@gmail.com>
2016-09-15 10:03:08 +0300

    Add project files.

---
+++ TestLibrary/LoadData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class LoadData
+    {
+        public void Run()
+        {
+            //using (var db = new AdventureWorks2008Entities())
+            //{
+            //    var query = from p in db.People orderby p.FirstName select p;
+            //    foreach (var r in query)
+            //    {
+            //        Console.WriteLine("Read person: FirstName: {0} LastName: {1}", r.FirstName, r.LastName);
+            //    }
+            //}
+        }
+    }
+}
---
+++ UnitTester/TestLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTester
+{
+    [TestClass]
+    public class TestLoader
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
